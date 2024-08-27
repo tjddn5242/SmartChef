3f21f02ef6e3b0fa74b508a8783a0f0f1c07fff8
김성우 <tjddn5242@gmail.com>
2024-08-27 22:12:12 +0900

    no message

--- test.py
+++
@@ -1,8 +0,0 @@
-from llmStructure import *
-
-# user input
-user_need = '뜨끈한 해물이 포함된 국물이 있는 요리.'
-ingredients = '오징어| 새우| 어묵| 조개| 홍합| 고추가루'
-disease = '당뇨병'
-
-print(gptOutput(user_need, ingredients, disease))
